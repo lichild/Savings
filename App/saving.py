06c650f46ea08725ab4fe6efd0e2fd1d3fb626d7
Frank Li <lzaasmt@gmail.com>
2021-10-28 13:08:48 -0600

    Savings version 0.0.1

---
+++ App/saving.py
@@ -0,0 +1,14 @@
+import os
+
+# Clear the screen
+os.system('clear')
+# Display title bar 
+print("\t*********************************************************")
+print("\t***               Manage Your Money                   ***")
+print("\t*********************************************************")
+
+
+user_input = input("Options:")
+while user_input != "Exit" and user_input != "exit":
+    user_input = input("Options:")
+
